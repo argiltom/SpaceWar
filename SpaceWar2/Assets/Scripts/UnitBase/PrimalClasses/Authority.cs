980cd6d6764becf92a8f426a6977ba37c3cfb4b0
argiltom <52417168+argiltom@users.noreply.github.com>
2022-09-03 21:27:11 +0900

    flushingの間隔を減らした

--- SpaceWar2/Assets/Scripts/UnitBase/PrimalClasses/Authority.cs
+++ SpaceWar2/Assets/Scripts/UnitBase/PrimalClasses/Authority.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Collections.Concurrent;//上手く使えるか分からないな・・・
 /// <summary>
 /// 艦隊指揮・披指揮 艦同士の関係が木構造で構成されるようになる
 /// </summary>
@@ -119,7 +120,7 @@ public class Authority : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        flushingTimer = DelayTimer.DelayTimerConstructor(gameObject,1);
+        flushingTimer = DelayTimer.DelayTimerConstructor(gameObject, 5);
         
     }
 
